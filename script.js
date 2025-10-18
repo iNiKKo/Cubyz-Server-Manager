4690b1d65b73875e2adf120e72bfd9a13a74e408
Nick <67593147+iNiKKo@users.noreply.github.com>
2025-10-19 00:11:54 +0100

    tessst

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const API_URL = 'http://api.ashframe.net/update';
+const API_URL = 'https://api.ashframe.net/update';
 const CURRENT_SCRIPT_VERSION = '1.2';
 
 async function fetchServerData() {
   try {
-    const response = await fetch(API_URL, {
-      headers: { 'ngrok-skip-browser-warning': 'true' }
-    });
+    const response = await fetch(API_URL); 
     if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
 
     const servers = await response.json();
@@ -35,24 +33,20 @@ async function fetchServerData() {
       const ip = info.ip;
       const iconURL = info.icon || "https://cdn-icons-png.flaticon.com/512/10091/10091152.png";
 
-
       div.innerHTML = `
-  <div class="server-icon-wrapper">
-    <img class="server-icon" src="${iconURL}" alt="Server Icon" />
-  </div>
-  <h2>${serverId}
-    <span class="status-badge ${isOffline ? 'offline' : 'online'}">${isOffline ? 'Offline' : 'Online'}</span>
-    ${!isOffline && isOutdated ? '<span class="status-badge outdated">Outdated</span>' : ''}
-  </h2>
-  ${ip ? `<p><span class="label">IP:</span> <span class="value server-ip">${ip}</span></p>` : ''}
-  <p><span class="label">Gamemode:</span> <span class="value">${info.gamemode ?? 'Unknown'}</span></p>
-  <p><span class="label">Players online:</span> <span class="value">${isOffline ? '0' : info.player_count}</span></p>
-  <p><span class="label">Players:</span> <span class="value">${isOffline ? 'N/A' : playersList}</span></p>
-  <p><span class="label">Deaths:</span> <span class="value">${info.death_count ?? 0}</span></p>
-  
-
-`;
-
+        <div class="server-icon-wrapper">
+          <img class="server-icon" src="${iconURL}" alt="Server Icon" />
+        </div>
+        <h2>${serverId}
+          <span class="status-badge ${isOffline ? 'offline' : 'online'}">${isOffline ? 'Offline' : 'Online'}</span>
+          ${!isOffline && isOutdated ? '<span class="status-badge outdated">Outdated</span>' : ''}
+        </h2>
+        ${ip ? `<p><span class="label">IP:</span> <span class="value server-ip">${ip}</span></p>` : ''}
+        <p><span class="label">Gamemode:</span> <span class="value">${info.gamemode ?? 'Unknown'}</span></p>
+        <p><span class="label">Players online:</span> <span class="value">${isOffline ? '0' : info.player_count}</span></p>
+        <p><span class="label">Players:</span> <span class="value">${isOffline ? 'N/A' : playersList}</span></p>
+        <p><span class="label">Deaths:</span> <span class="value">${info.death_count ?? 0}</span></p>
+      `;
 
       if (ip) {
         div.title = `Click to copy IP: ${ip}`;
